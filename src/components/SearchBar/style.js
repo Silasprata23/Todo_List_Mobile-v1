e8aaeaec1b23ef3ba3ce97a9564eb4e616987834
Silasprata23 <silasprata23@gmail.com>
2026-04-16 10:21:49 -0300

    feat: add SearchBar component with input styling and functionality

---
+++ src/components/SearchBar/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../styles/global";
+
+export const styles = StyleSheet.create({
+
+    input: {
+        backgroundColor: theme.colors.white,
+        padding: 15,
+        borderWidth: 1,
+        borderColor: theme.colors.border,
+        borderRadius: 8,
+        fontSize: 16,
+        marginBottom: 20,
+
+    }
+});
