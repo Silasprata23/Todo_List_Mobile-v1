0e8b849f0f1bb3dd12ac9af94eca98beac814ca2
Silasprata23 <silasprata23@gmail.com>
2026-04-16 10:21:35 -0300

    feat: implement EditTask component with modal for editing tasks

---
+++ src/components/EditTask/index.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react";
+import { Modal, View, Text, TextInput, TouchableOpacity } from "react-native";
+import style from "../AddTask/style";
+
+import {styles} from "./style";
+
+export default function EditTask({ visible, onClose, taskData, onSave }) {
+    const [currentTask, setCurrentTask] = useState(taskData);
+
+    useEffect(() => {
+        setCurrentTask(taskData);
+    }, [taskData]);
+
+    return
+    (
+        <Modal visible={visible} animationType='fade'>
+            <View style={style.modal}>
+                <Text style={style.titulo}>Editar Tarefa</Text>
+                <TextInput
+                    value={currentTask?.title}
+                    style={style.input}
+                    onChangeText={t => setCurrentTask({...currentTask, title: t})}
+                />
+                
+                <TextInput
+                    value={currentTask?.description}
+                    style={style.input}
+                    onChangeText={t => setCurrentTask({...currentTask, description: t})}
+                />
+
+                <TouchableOpacity style={style.button} onPress={() => onSave(currentTask)}>
+                    <Text>Atualizar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={style.button}  onPress={onClose}>
+                    <Text>Voltar</Text>
+                    
+                </TouchableOpacity>
+            </View>
+        </Modal>
+    )
+}
---
+++ src/components/EditTask/style.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../styles/global";
+
+export const styles = StyleSheet.create({
+    modal: {
+        flex: 1,
+        padding: 30,
+        backgroundColor: theme.colors.background,
+    },
+    titulo: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        backgroundColor: '#fff',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        marginBottom: 10,
+        borderRadius: 8,
+        
+    },
+    button: {
+        backgroundColor: theme.colors.primary,
+        padding: 15,
+        borderRadius: 8,
+        alignItems: 'center',
+        marginBottom: 10,
+    }
+})
